b5d5c2cad3d242563cf0ea22132b3b75f21f985a
vinicius-asf <viniciusant3@gmail.com>
2020-11-07 12:09:22 -0300

    atualizar e remover pacientes

--- nutri_cli/lista_paciente.py
+++ nutri_cli/lista_paciente.py
@@ -9,45 +9,77 @@ def qtd_telefones(text = False):
         return qtd_telefones('Insira somente numeros inteiros: ')
     else:
         if qtd <= 0:
-            return qtd_telefones('Insira um número maior do que zero:')
+            return qtd_telefones('Insira um número maior do que zero: ')
     return qtd
 
 class Lista_Paciente():
     lista_de_pacientes = [
-        {'nome':'vinicius'},
-        {'nome':'vitor'},
+        {
+            'nome': 'vinicius',
+            'email': 'vini@gmail.com'
+        },
+        {
+            'nome': 'vitor',
+            'email': 'vitor@hotmail.com'
+        },
     ]
 
     def insere_paciente(self):
         """
-        docstring
+        Inicia o processo de inserção de um paciente na lista
         """
         paciente = {}
         paciente['nome'] = input('Nome: ')
         paciente['endereco'] = input('Endereço: ')
         n_telefones = qtd_telefones()
         paciente['telefones'] = [input('Telefone {}: '.format(x)) for x in range(1,n_telefones+1)]
-        paciente['e-mail'] = input('E-mail: ')
+        paciente['email'] = input('Email: ')
         paciente['nascimento'] = input('Data de nascimento: ')
         self.lista_de_pacientes.append(paciente)
 
+    def print_paciente(self, nome):
+        """
+        Lista no console os dados de pacientes com o nome dado
+        """
+        lista = [paciente for paciente in self.lista_de_pacientes if paciente['nome'].startswith(nome)]
+        if len(lista) > 0:
+            for paciente in lista:
+                print('ID:{}'.format(lista.index(paciente)))
+                for param, val in paciente.items():
+                    print('{}:{}\t'.format(param,val), end=' ')
+                print('\n____________________________________________')
+
     def retorna_paciente(self, nome=False):
+        """
+        Retorna uma lista de pacientes com o nome dado ou a lista toda caso não houver o que procurar
+        """
         if nome:
+            self.print_paciente(nome)
             return [paciente for paciente in self.lista_de_pacientes if paciente['nome'].startswith(nome)]
         else:
+            self.print_paciente('')
             return self.lista_de_pacientes
     
-    def atualiza_paciente(self, parameter_list):
+    def atualiza_paciente(self, paciente):
         """
-        docstring
+        Atualiza os dados do paciente e retorna os novos dados
         """
-        pass
+        novo_paciente = {}
+        novo_paciente['nome'] = input('Nome: ')
+        novo_paciente['endereco'] = input('Endereço: ')
+        n_telefones = qtd_telefones()
+        novo_paciente['telefones'] = [input('Telefone {}: '.format(x)) for x in range(1,n_telefones+1)]
+        novo_paciente['email'] = input('Email: ')
+        novo_paciente['nascimento'] = input('Data de nascimento: ')
+        
+        self.lista_de_pacientes[self.lista_de_pacientes.index(paciente)] = novo_paciente
+        return novo_paciente
     
-    def deleta_paciente(self, parameter_list):
+    def deleta_paciente(self, paciente):
         """
-        docstring
+        Deleta o paciente da lista de pacientes. Remove os dados de consultas também
         """
-        pass
+        return self.lista_de_pacientes.pop(self.lista_de_pacientes.index(paciente))
 
     def limpa_lista(self, parameter_list):
         """
--- nutri_cli/nutri_cli.py
+++ nutri_cli/nutri_cli.py
@@ -2,10 +2,24 @@ import cmd
 
 from nutri_cli.lista_paciente import Lista_Paciente
 
+def seleciona_item_lista(lista, texto = False):
+    try:
+        if texto:
+            idx = int(input(texto))
+        else:
+            idx = int(input('Selecione um ID: '))
+    except ValueError as identifier:
+        return seleciona_item_lista(lista, 'Insira somente numeros inteiros: ')
+    else:
+        if idx > len(lista):
+            return seleciona_item_lista(lista, 'Insira um número correto: ')
+    return idx
+
 class NutriCLI(cmd.Cmd):
     intro = 'Bem vindo ao Nutri CLI, Marina! Digite help ou ? para ver a lista de comandos\n'
-    active = 'paciente'
-    prompt = '({}): '.format(active)
+    active = {'nome' : 'paciente'}
+    idp = 0
+    prompt = '({}): '.format(active['nome'])
     pacientes = Lista_Paciente()
 
     # funções basicas
@@ -14,18 +28,21 @@ class NutriCLI(cmd.Cmd):
         print('Obrigado por usar o Nutri CLI')
         return True
 
+    def atualizar_prompt(self, active):
+        self.active = active
+        self.prompt = '({}): '.format(self.active['nome'])
+
     # funções de pacientes
 
     def do_paciente(self, args):
         """
-        Ações para criar, atualizar, selecionar e remover pacientes do sistema\n
-        \t: -s <nome do paciente> >> retorna uma lista de nomes relacionados ao nome dado\n
-        \t\tse houver apenas um nome na lista, este será selecionado\n
-        \t: -s <nome do paciente> <numero> >> seleciona o paciente com o numero da posição na lista
+        Ações para criar, atualizar, selecionar e remover pacientes do sistema
+        \t: -s <nome do paciente> >> seleciona paciente a partir de uma lista de nomes parecidos ao nome dado
         \t\tse houver apenas um nome na lista, este será selecionado\n
         \t: -u >> atualiza os dados do paciente ativo\n
         \t: -d >> deleta os dados do paciente ativo\n
         \t: -c >> cria o paciente na lista de pacientes\n
+        \t: -f >> fecha o paciente ativo\n
         """
 
         try:
@@ -35,42 +52,66 @@ class NutriCLI(cmd.Cmd):
         except IndexError as identifier:
             if not self.pacientes.retorna_paciente():
                 print('Lista vazia - insira pacientes para começar')
-            else:
-                print(self.pacientes.retorna_paciente())
         
         else:
             if acao == '-c': # criar paciente
                 self.pacientes.insere_paciente()
 
             elif acao == '-s': # selecionar paciente
+                list_nomes = []
                 print('selecionar paciente')
-                if len(res) != 2:
+                if len(res) < 2:
                     res.append((input('Nome do paciente: ')))
-                
+                    # list_nomes = self.pacientes.retorna_paciente(res[1])
+
                 list_nomes = self.pacientes.retorna_paciente(res[1])
                 if len(list_nomes) > 1:
-                    print(self.pacientes.retorna_paciente(res[1]))
+                    # print(self.pacientes.retorna_paciente(res[1]))
+                    self.idp = seleciona_item_lista(list_nomes)
+                    self.atualizar_prompt(list_nomes[self.idp])
                 elif len(list_nomes) == 1:
-                    self.active = list_nomes[0]['nome']
-                    self.prompt = '({}): '.format(self.active)
+                    self.idp = 0
+                    self.atualizar_prompt(list_nomes[self.idp])
                 else:
                     print('não foi encontrado pacientes com {}'.format(res[1]))
-                    # TODO: iniciar o processo de criação de paciente
-
+                    res = input('deseja iniciar o processo de criação? (s)(n)').lower()
+                    if res == 's' or res == 'sim':
+                        self.atualizar_prompt(self.pacientes.insere_paciente()) #TODO ao inserir, retorna a posição do paciente na lista de nomes
+                    elif res == 'n' or res == 'nao':
+                        return
+                    else:
+                        print('instrução não foi clara - retornando para o inicio')
 
                 # self.active = paciente
                 # self.prompt = '({}): '.format(self.active)
             
             elif acao == '-u': # atualizar paciente
                 print('atualizar paciente')
-            
+                # print(self.active)
+
+                if self.active == 'paciente':
+                    print('selecione um paciente primeiro')
+                    return
+                else:
+                    self.active = self.pacientes.atualiza_paciente(self.active)
+                    self.prompt = self.active['nome']
+
             elif acao == '-d': # deletar paciente
                 print('deletar paciente')
+                res = input('não tem como recuperar os dados. Deseja continuar? (s)(n)').lower()
+                if res == 's' or res == 'sim':
+                    print(self.pacientes.deleta_paciente(self.active)['nome'], ' foi deletado') #TODO ao deletar, deletar também as consultas
+                    return self.atualizar_prompt({'nome' : 'paciente'})
+                elif res == 'n' or res == 'nao':
+                    return
+                else:
+                    print('instrução não foi clara - retornando para o inicio')
             
             elif acao == '-f': # fechar paciente
-                self.active = 'paciente'
-                self.prompt = '({}): '.format(self.active)
+                return self.atualizar_prompt({'nome' : 'paciente'})
 
+            else:
+                print('instrução não foi clara - digite: help paciente')
     # funções de consultas
 
     def do_consulta(self, args):
@@ -82,7 +123,6 @@ class NutriCLI(cmd.Cmd):
         \t: -u <numero> >> atualiza os dados da consulta com o índice indicado\n
         \t: -d >> deleta as consultas do paciente ativo\n
         """
-
         if self.active == 'paciente':
             print('selecione um paciente primeiro')
             return
